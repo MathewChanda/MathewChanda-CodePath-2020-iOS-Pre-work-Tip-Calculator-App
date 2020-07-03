e7f3fa074722e22cf408ecf5cc542e178c346826
Mathew Chanda <mvchanda@ncsu.edu>
2020-07-03 06:38:13 -0400

    Added keyboard dissimal in the home view

--- Tipper/Tipper/ViewController.swift
+++ Tipper/Tipper/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
+    
 }
 
