a79d5252f9882ba4a27f2cf1a63193343612ca0a
Mathew Chanda <mvchanda@ncsu.edu>
2020-07-03 07:29:39 -0400

    Added calculating tip feature in the home view

--- Tipper/Tipper/ViewController.swift
+++ Tipper/Tipper/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    /*
+     *  Dismiss the keyboard by tapping the view 
+     */
     @IBAction func onTap(_ sender: Any) {
         print("Hello")
+        
         view.endEditing(true)
+    }
+    
+    /*
+     * Calculate tip based on the value in the textfield and segmented control
+     */
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate the total and tip
+        let tipPercentages = [0.15, 0.18 ,0.20]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total
+        tipLabel.text = String(format : "%.2f", tip)
+        totalLabel.text = String(format: "%.2f" , total)
     }
     
 }
